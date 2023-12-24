ac72a08e7752a17e4a545eb6d9c5ee3b7032fc93
ollvr <omarobouhawelo15@gmail.com>
2023-12-24 06:05:03 +0100

    updates

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const validUrl = require('valid-url');
 const app = express();
 
-// Basic Configuration
+const bodyParser = require('body-parser');
+
+const urlDatabase = [];
+
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -18,6 +24,37 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+
+app.post('/api/shorturl', (req, res) => {
+  const { url } = req.body; 
+  
+  // Check if the URL is valid
+  if (!validUrl.isWebUri(url)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  
+  const shortUrl = urlDatabase.length + 1; 
+  urlDatabase.push({ original_url: url, short_url: shortUrl });
+
+  return res.json({ original_url: url, short_url: shortUrl });
+});
+
+// GET endpoint to redirect to the original URL
+app.get('/api/shorturl/:shortUrl', (req, res) => {
+  const shortUrl = parseInt(req.params.shortUrl, 10);
+
+  const urlEntry = urlDatabase.find(entry => entry.short_url === shortUrl);
+
+  if (urlEntry) {
+    return res.redirect(urlEntry.original_url);
+  } else {
+    return res.status(404).json({ error: 'No short URL found for the given input' });
+  }
+});
+
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
